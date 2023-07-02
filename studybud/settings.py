544b4a2b403268a0efb6cd85aa763841ccd1c025
OSHI PAULINUS <61438779+Eminentpaul@users.noreply.github.com>
2023-07-02 18:50:01 +0100

    Update settings.py
    Adding domain name to the settings

--- studybud/settings.py
+++ studybud/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-w37f2^efv%45!1s$#td+lm(n68hn7gggbrmnitqdbry+wa-o)0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
